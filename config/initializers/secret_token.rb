fb5e77be645345df645dbe4ef613625cee6fa108
Bhanu Prasad <bhanuprasad143@gmail.com>
2012-07-09 14:44:37 +0700

    create app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StartupShopKC::Application.config.secret_token = '9f5be4faa24cfb92353a49ac4e2221b5f5a985d5853441e4c4fbe22826b6293b878b37837da0f972fd94c4c047663d18ab2ed8ebddd42dd992ce60af5136c5b8'
