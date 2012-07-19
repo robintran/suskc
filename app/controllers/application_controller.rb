bb4e1c4ece2935ecb7249d13cc44270dabe12a07
Bhanu Prasad <bhanuprasad143@gmail.com>
2012-07-19 09:37:41 +0700

    authenticate user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
-  helper_method :current_user
+  helper_method :current_user, :authenticate_user
   
   def current_user
     return session[:user_id] ? User.find(session[:user_id]) : nil
+  end
+  
+  def authenticate_user
+    unless session[:user_id]
+      flash[:alert] = "Please login first"
+      redirect_to "/signin"
+    end
   end
   
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
   def new
+    @errors = [flash[:alert]]
   end
 
   def create
