64af885bfeb9d3ad2d5dffc5f078b16227973a1f
Bhanu Prasad <bhanuprasad143@gmail.com>
2012-07-20 09:02:52 +0700

    config autoload path

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module StartupShopKC
     # -- all .rb files in that directory are automatically loaded.
 
     # Custom directories with classes and modules you want to be autoloadable.
-     config.autoload_paths += %W(#{config.root}/lib)
+     config.autoload_paths += %W(#{config.root}/lib/classes)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
